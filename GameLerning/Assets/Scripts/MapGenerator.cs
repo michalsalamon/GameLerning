d199293bdf7e26991804d659d6c0f6113d9b23f0
michalsalamon <99210092+michalsalamon@users.noreply.github.com>
2022-03-18 16:19:21 +0100

    lesson e10

--- GameLerning/Assets/Scripts/MapGenerator.cs
+++ GameLerning/Assets/Scripts/MapGenerator.cs
@@ -9,11 +9,15 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private Vector2 mapSize;
 
     [SerializeField] [Range(0, 1)] private float outlinePercent;
+    [SerializeField] [Range(0, 1)] private float obstacleProcent;
+
+    [SerializeField] private float tileSize;
 
     List<Coord> allTileCoords;
     Queue<Coord> shuffledTileCoords;
 
     [SerializeField] private int seed = 10;
+    private Coord mapCenter;
 
     private void Start()
     {
@@ -31,7 +35,7 @@ public class MapGenerator : MonoBehaviour
             }
         }
         shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
-
+        mapCenter = new Coord((int)mapSize.x / 2, (int)mapSize.y / 2);
 
                 string holderName = "Generated Map";
         if (transform.Find(holderName))
@@ -48,25 +52,81 @@ public class MapGenerator : MonoBehaviour
             {
                 Vector3 f_tilePosition = CoordToPosition(x, y);
                 Transform newTile = Instantiate(tilePrefab, f_tilePosition, Quaternion.Euler(Vector3.right * 90));
-                newTile.localScale *= (1 - outlinePercent);
+                newTile.localScale = Vector3.one * (1 - outlinePercent) * tileSize;
                 newTile.transform.parent = mapHolder;
             }
         }
 
-        int f_obstacleCount = 10;
+
+        bool[,] f_obstacleMap = new bool[(int)mapSize.x, (int)mapSize.y];
+        int f_obstacleCount = (int)(mapSize.x * mapSize.y * obstacleProcent);
+        int f_currentObstacleCount = 0;
+
         for (int i = 0; i < f_obstacleCount; i++)
         {
             Coord f_randomCoord = GetRandomCoord();
-            Vector3 f_obstaclePosition = CoordToPosition(f_randomCoord.x, f_randomCoord.y);
-            Transform f_newObstacle = Instantiate(obstaclePrefab, f_obstaclePosition + Vector3.up * 0.5f, Quaternion.identity);
-            f_newObstacle.transform.parent = mapHolder;
+            f_obstacleMap[f_randomCoord.x, f_randomCoord.y] = true;
+            f_currentObstacleCount ++;
+
+            if (f_randomCoord != mapCenter && MapIsFullyAccessible(f_obstacleMap, f_currentObstacleCount))
+            {
+                Vector3 f_obstaclePosition = CoordToPosition(f_randomCoord.x, f_randomCoord.y);
+
+
+                Transform f_newObstacle = Instantiate(obstaclePrefab, f_obstaclePosition + Vector3.up * 0.5f * tileSize, Quaternion.identity);
+                f_newObstacle.localScale = Vector3.one * tileSize;
+                f_newObstacle.transform.parent = mapHolder;
+            }
+            else
+            {
+                f_obstacleMap[f_randomCoord.x, f_randomCoord.y] = false;
+                f_currentObstacleCount--;
+            }
         }
 
     }
 
+    private bool MapIsFullyAccessible(bool[,] f_obstacleMap, int f_currentObstacleCount)
+    {
+        bool[,] f_mapFlags = new bool[f_obstacleMap.GetLength(0), f_obstacleMap.GetLength(1)];
+        Queue<Coord> f_queue = new Queue<Coord>();
+        f_queue.Enqueue(mapCenter);
+        f_mapFlags[mapCenter.x, mapCenter.y] = true;
+
+        int f_accsessibleTileCount = 1;
+
+        while (f_queue.Count > 0)
+        {
+            Coord f_tile = f_queue.Dequeue();
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    int f_neighbourX = f_tile.x + x;
+                    int f_neighbourY = f_tile.y + y;
+                    if (x == 0 || y == 0)
+                    {
+                        if (f_neighbourX >= 0 && f_neighbourX < f_obstacleMap.GetLength(0) && f_neighbourY >= 0 && f_neighbourY < f_obstacleMap.GetLength(1))
+                        {
+                            if (!f_mapFlags[f_neighbourX, f_neighbourY] && !f_obstacleMap[f_neighbourX, f_neighbourY])
+                            {
+                                f_mapFlags[f_neighbourX, f_neighbourY] = true;
+                                f_queue.Enqueue(new Coord(f_neighbourX, f_neighbourY));
+                                f_accsessibleTileCount ++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int f_targetAccessibleTileCount = (int)(mapSize.x * mapSize.y - f_currentObstacleCount);
+        return f_targetAccessibleTileCount == f_accsessibleTileCount;
+    }
+
     private Vector3 CoordToPosition(int f_x, int f_y)
     {
-        return new Vector3(-mapSize.x / 2 + 0.5f + f_x, 0, -mapSize.y / 2 + 0.5f + f_y);
+        return new Vector3(-mapSize.x / 2 + 0.5f + f_x, 0, -mapSize.y / 2 + 0.5f + f_y) * tileSize;
     }
 
     private Coord GetRandomCoord()
@@ -86,6 +146,16 @@ public class MapGenerator : MonoBehaviour
         {
             x = f_x;
             y = f_y;
+        }
+
+        public static bool operator ==(Coord c1, Coord c2)
+        {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1 == c2);
         }
     }
 }
